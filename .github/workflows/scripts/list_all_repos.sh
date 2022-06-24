c04d270054362fd86ff033d0faab0dc426f62ef5
Andy Pearson <andy@londonparkour.com>
2022-06-24 20:59:41 +0100

    tsv

--- .github/workflows/scripts/list_all_repos.sh
+++ .github/workflows/scripts/list_all_repos.sh
@@ -8,16 +8,23 @@ PAT=$1;
 /usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, REPO: .full_name, URL: .html_url, LANG: ( "https://api.github.com/repos/" + .full_name + "/languages"), DESC: .description, DATE: .created_at, TAGS: "%REPLACE%" , READ: ("https://raw.githubusercontent.com/" + .full_name + "/master/README.md") }  ' > ./repos.json
 /usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&page=2&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, REPO: .full_name, URL: .html_url, LANG: ( "https://api.github.com/repos/" + .full_name + "/languages") , DESC: .description, DATE: .created_at, TAGS: "%REPLACE%" , READ: ("https://raw.githubusercontent.com/" + .full_name + "/master/README.md") }  ' >> ./repos.json
 
-
+cat ./repos.json
 # Loop JSON and get languages
 # -r for raw. (Removes quotes around strings)
 # capitalises the language also.
 jq -r '.LANG' ./repos.json | while read url; do
     /usr/bin/curl -u ${GHUSER}:${PAT} -s "${url}" > languages.json
+    cat languages.json
+
     main_language=$(cat languages.json | jq -r 'keys | last(.[])')
     tags=$(cat languages.json | jq -c -r 'keys | . | @tsv')
-    echo $tags
+
+    echo "lang: ${main_language}"
+    echo "tags: ${tags}"
+    echo "---"
+
     lowercased=${main_language,,}
+
     sed -i "s|${url}|${lowercased}|g" ./repos.json
     sed -i "s|%REPLACE%|${tags}|g" ./repos.json
 done
