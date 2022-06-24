d988d7b4dc37fa75b89704d8cb3671c6e6d361a7
Andy Pearson <andy@londonparkour.com>
2022-06-24 21:10:27 +0100

    quoted

--- .github/workflows/scripts/list_all_repos.sh
+++ .github/workflows/scripts/list_all_repos.sh
@@ -5,10 +5,8 @@ GHUSER=IOROOT;
 PAT=$1;
 
 # Grab TWO PAGES of the Repo list from Github API
-/usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, REPO: .full_name, URL: .html_url, LANG: ( "https://api.github.com/repos/" + .full_name + "/languages"), DESC: .description, DATE: .created_at, TAGS: "%REPLACE%" , READ: ("https://raw.githubusercontent.com/" + .full_name + "/master/README.md") }  ' > ./repos.json
-/usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&page=2&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, REPO: .full_name, URL: .html_url, LANG: ( "https://api.github.com/repos/" + .full_name + "/languages") , DESC: .description, DATE: .created_at, TAGS: "%REPLACE%" , READ: ("https://raw.githubusercontent.com/" + .full_name + "/master/README.md") }  ' >> ./repos.json
-
-cat ./repos.json
+/usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, REPO: .full_name, URL: .html_url, LANG: ( "https://api.github.com/repos/" + .full_name + "/languages"), DESC: .description, DATE: .created_at, TAGS: "REPLACE" , READ: ("https://raw.githubusercontent.com/" + .full_name + "/master/README.md") }  ' > ./repos.json
+/usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&page=2&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, REPO: .full_name, URL: .html_url, LANG: ( "https://api.github.com/repos/" + .full_name + "/languages") , DESC: .description, DATE: .created_at, TAGS: "REPLACE" , READ: ("https://raw.githubusercontent.com/" + .full_name + "/master/README.md") }  ' >> ./repos.json
 
 # Loop JSON and get languages
 # -r for raw. (Removes quotes around strings)
@@ -30,5 +28,6 @@ jq -r '.LANG' ./repos.json | while read url; do
     lowercased=${main_language,,}
 
     sed -i "s|${url}|${lowercased}|g" ./repos.json
-    sed -i "s|%REPLACE%|${tags}|g" ./repos.json
+    sed -i "s|REPLACE|"${tags}"|g" ./repos.json
+
 done
