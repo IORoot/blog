257c7cceb647542455b198151928e8dfe3d5c655
Andy Pearson <andy@londonparkour.com>
2022-06-24 21:03:23 +0100

    tsv

--- .github/workflows/scripts/list_all_repos.sh
+++ .github/workflows/scripts/list_all_repos.sh
@@ -9,6 +9,7 @@ PAT=$1;
 /usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&page=2&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, REPO: .full_name, URL: .html_url, LANG: ( "https://api.github.com/repos/" + .full_name + "/languages") , DESC: .description, DATE: .created_at, TAGS: "%REPLACE%" , READ: ("https://raw.githubusercontent.com/" + .full_name + "/master/README.md") }  ' >> ./repos.json
 
 cat ./repos.json
+
 # Loop JSON and get languages
 # -r for raw. (Removes quotes around strings)
 # capitalises the language also.
@@ -19,6 +20,7 @@ jq -r '.LANG' ./repos.json | while read url; do
     main_language=$(cat languages.json | jq -r 'keys | last(.[])')
     tags=$(cat languages.json | jq -c -r 'keys | . | @tsv')
 
+    echo "url: ${url}"
     echo "lang: ${main_language}"
     echo "tags: ${tags}"
     echo "---"
