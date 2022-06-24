4c4570573fec775fa2b060b783fa22e3e776e619
Andy Pearson <andy@londonparkour.com>
2022-06-24 19:41:50 +0100

    PAT

--- .github/workflows/scripts/list_all_repos.sh
+++ .github/workflows/scripts/list_all_repos.sh
@@ -8,13 +8,12 @@ PAT=$1;
 /usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, REPO: .full_name, URL: .html_url, LANG: ( "https://api.github.com/repos/" + .full_name + "/languages"), DESC: .description, DATE: .created_at, READ: ("https://raw.githubusercontent.com/" + .full_name + "/master/README.md") }  ' > ./repos.json
 /usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&page=2&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, REPO: .full_name, URL: .html_url, LANG: ( "https://api.github.com/repos/" + .full_name + "/languages") , DESC: .description, DATE: .created_at, READ: ("https://raw.githubusercontent.com/" + .full_name + "/master/README.md") }  ' >> ./repos.json
 
+
 # Loop JSON and get languages
 # -r for raw. (Removes quotes around strings)
 # capitalises the language also.
 jq -r '.LANG' ./repos.json | while read url; do
-    # repo_language=$(/usr/bin/curl -s "${url}" --header 'authorization: Bearer ${{ secrets.GITHUB_TOKEN }}' | jq -r 'keys | last(.[])')
     /usr/bin/curl -u ${GHUSER}:${PAT} -s "${url}" > languages.json
-    cat languages.json
     repo_language=$(cat languages.json | jq -r 'keys | last(.[])')
     lowercased=${repo_language,,}
     sed -i "s|${url}|${lowercased}|g" ./repos.json
