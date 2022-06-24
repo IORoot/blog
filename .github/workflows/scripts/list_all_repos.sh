cf1f52c368feea8c7ba359d19e1d7f3f6de47097
Andy Pearson <andy@londonparkour.com>
2022-06-24 20:22:47 +0100

    tags

--- .github/workflows/scripts/list_all_repos.sh
+++ .github/workflows/scripts/list_all_repos.sh
@@ -5,8 +5,8 @@ GHUSER=IOROOT;
 PAT=$1;
 
 # Grab TWO PAGES of the Repo list from Github API
-/usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, REPO: .full_name, URL: .html_url, LANG: ( "https://api.github.com/repos/" + .full_name + "/languages"), DESC: .description, DATE: .created_at, READ: ("https://raw.githubusercontent.com/" + .full_name + "/master/README.md") }  ' > ./repos.json
-/usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&page=2&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, REPO: .full_name, URL: .html_url, LANG: ( "https://api.github.com/repos/" + .full_name + "/languages") , DESC: .description, DATE: .created_at, READ: ("https://raw.githubusercontent.com/" + .full_name + "/master/README.md") }  ' >> ./repos.json
+/usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, REPO: .full_name, URL: .html_url, LANG: ( "https://api.github.com/repos/" + .full_name + "/languages"), DESC: .description, DATE: .created_at, TAGS: "%REPLACE%" , READ: ("https://raw.githubusercontent.com/" + .full_name + "/master/README.md") }  ' > ./repos.json
+/usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&page=2&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, REPO: .full_name, URL: .html_url, LANG: ( "https://api.github.com/repos/" + .full_name + "/languages") , DESC: .description, DATE: .created_at, TAGS: "%REPLACE%" , READ: ("https://raw.githubusercontent.com/" + .full_name + "/master/README.md") }  ' >> ./repos.json
 
 
 # Loop JSON and get languages
@@ -14,7 +14,10 @@ PAT=$1;
 # capitalises the language also.
 jq -r '.LANG' ./repos.json | while read url; do
     /usr/bin/curl -u ${GHUSER}:${PAT} -s "${url}" > languages.json
-    repo_language=$(cat languages.json | jq -r 'keys | last(.[])')
-    lowercased=${repo_language,,}
+    main_language=$(cat languages.json | jq -r 'keys | last(.[])')
+    tags=$(cat languages.json | jq -r 'keys | . | @sh')
+    lowercased=${main_language,,}
+    tags_lower=${tags,,}
     sed -i "s|${url}|${lowercased}|g" ./repos.json
+    sed -i "s|%REPLACE%|${tags_lower}|g" ./repos.json
 done
